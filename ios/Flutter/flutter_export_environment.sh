874f42d04fd503f214504afa376eb487b64bef1d
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-03-11 10:12:11 +0100

    Update app to current state

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/matthiaszarzecki/projects/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/matthiaszarzecki/projects/flutter/WalkingApp"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/matthiaszarzecki/projects/flutter/WalkingApp/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/matthiaszarzecki/projects/flutter/WalkingApp/.dart_tool/package_config.json"
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -2,8 +2,15 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_pedometer/FlutterPedometerPlugin.h>)
 #import <flutter_pedometer/FlutterPedometerPlugin.h>
+#else
+@import flutter_pedometer;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
